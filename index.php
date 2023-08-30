b871944d5a3acd006dd3b809141203047bd6f1be
MIU-cz <0151300@gmail.com>
2023-08-30 16:29:35 +0200

    init: start project add docs

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="cs">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>3IT PHP-Test - MIU</title>
+	<link rel="shortcut icon" href="src/x192-150x150.png" type="image/png">
+
+</head>
+
+<body>
+
+</body>
+
+</html>
