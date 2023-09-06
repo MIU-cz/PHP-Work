8d9dc9fe2747e6d2058389a2d0651e73af675040
MIU-cz <0151300@gmail.com>
2023-09-06 11:49:19 +0200

    feat: add main styles

---
+++ ibaCZ-Blog/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>IBACZ - MIU - cube practical demo</title>
+	<link rel="shortcut icon" href="./src/img/apple-touch-icon-152x152.png" type="image/png">
+	<link rel="stylesheet" href="./src/css/style.css">
+</head>
+
+<body>
+
+</body>
+
+</html>
