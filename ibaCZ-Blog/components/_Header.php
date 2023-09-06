0580732f8fdab1e0c85bacdd5dbfacb9de13687e
MIU-cz <0151300@gmail.com>
2023-09-06 23:20:06 +0200

    feat: Header completed

--- ibaCZ-Blog/components/_Header.php
+++ ibaCZ-Blog/components/_Header.php
@@ -27,15 +27,27 @@ require '_smm-bar.php';
 		</div>
 
 		<div class="btn_container" id="headerBtns">
-			<button class="rounded_btn header_btn" id="btnSearch">
+			<button class="rounded_btn header_btn" id="btnSearch" name="searchContainer">
 				<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
 					<path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
 				</svg>
 			</button>
-			<button class="rounded_btn header_btn" id="btnMenu"> </button>
+			<button class="rounded_btn header_btn" id="btnMenu" name="menuContainer"> </button>
 
-			<div class="search_container hide"></div>
-			<div class="menu_container hide"></div>
+			<div class="search_container hide" id="searchContainer">
+				<form action="" name="searchPost" method="post" class="search_form">
+					<input type="text" name="searchStr" class="searchStr" placeholder="what will we look for...">
+					<button type="submit" class="main_btn">go</button>
+				</form>
+
+			</div>
+
+			<div class="menu_container hide" id="menuContainer">
+				<!-- ?? --content-- ?? -->
+				<div class="menu_content">
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque esse repudiandae laborum eligendi quibusdam enim maxime laboriosam, aliquid modi deserunt nulla, alias cum porro suscipit asperiores facere veritatis eius similique.
+				</div>
+			</div>
 		</div>
 	</div>
 
--- ibaCZ-Blog/src/js/_header.js
+++ ibaCZ-Blog/src/js/_header.js
@@ -1,10 +1,31 @@
+const headerBtns = document.getElementById('headerBtns');
+
+headerBtns.addEventListener('click', (e) => {
+	e.stopPropagation();
+	const Btn = e.target;
+	const Container = document.getElementById(Btn.name);
+	Container.classList.toggle('hide');
+	Btn.classList.toggle('active');
+
+	if (Container.id !== 'searchContainer') {
+		Container.addEventListener('mouseleave', () => {
+			Container.classList.add('hide');
+			Btn.classList.remove('active');
+		});
+	}
+})
+
+
+
+// # main navBar Menu
+// ===================
 function showDropItems(event, key) {
 	event.preventDefault();
 
 	const dropItems = document.querySelector('[name=' + key + ']');
 	dropItems.classList.toggle('hide');
 
-	dropItems.addEventListener('mouseleave', (e) => {
-		e.target.classList.add('hide');
+	dropItems.addEventListener('mouseleave', () => {
+		dropItems.classList.add('hide');
 	});
 }
