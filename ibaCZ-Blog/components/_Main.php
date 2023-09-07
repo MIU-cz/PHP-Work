309fcf14eff7acb291ecf8afe93e3ba8c7c12d78
MIU-cz <0151300@gmail.com>
2023-09-07 14:05:51 +0200

    fix: aside section

--- ibaCZ-Blog/components/_Main.php
+++ ibaCZ-Blog/components/_Main.php
@@ -1,5 +1,6 @@
 <?php
 require '_tags-items.php';
+require '_smm-bar.php';
 
 $postItem = [
 	'tag' => '',
@@ -23,6 +24,12 @@ $postBtn = [
 ];
 
 $colPost = 10;
+
+$postItemAside = [
+	'foto' => './src/img/blog-foto-tmp.svg',
+	'title' => 'post item title',
+	'date' => date('d F Y', getDate()[0])
+];
 
 ?>
 
@@ -96,6 +103,86 @@ $colPost = 10;
 	</div>
 
 	<div class="aside__content">
+		<div class="aside_item_container brand_item">
+			<div class="brand_logo">
+				<img src="./src/img/logo-blog.svg" alt="logo-blog">
+			</div>
+			<div class="aside_text">
+				<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse id voluptate amet placeat beatae tempora eveniet officiis sint</p>
+			</div>
+			<div class="smm__links_container">
+				<ul class="links_content">
+					<?php
+					$smmBar($smmLinks);
+					?>
+				</ul>
+			</div>
+		</div>
+
+		<div class="aside_item_container post_item">
+			<div class="item_title">
+				<h3>header item</h3>
+			</div>
+
+			<?php
+			for ($i = 0; $i < 3; $i++) {
+				echo '
+				<div class="item">
+					<div class="item_foto">
+						<img src="' . $postItemAside['foto'] . '" alt="' . $postItemAside['foto'] . '">
+					</div>
+					<div class="item_description">
+						<h2>' . $postItemAside['title'] . '</h2>
+						<p>' . $postItemAside['date'] . '</p>
+					</div>
+				</div>
+				';
+			}
+			?>
+
+		</div>
+
+		<div class="aside_item_container topics_item">
+			<div class="item_title">
+				<h3>header item</h3>
+			</div>
+
+			<?php
+			for ($i = 0; $i < 7; $i++) {
+				echo '
+				<div class="topic_item">
+					<div class="item_deco">
+						<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16">
+							<path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z" />
+						</svg>
+					</div>
+					<div class="topic_title">topic-title-' . ($i + 1) . '</div>
+					<div class="topic_counts">(' . (round(rand(1, 10))) . ')</div>
+				</div>
+				';
+			}
+			?>
+
+
+		</div>
+
+		<div class="aside_item_container news_item">
+			<div class="item_title">
+				<h3>header item</h3>
+			</div>
+		</div>
+
+		<div class="aside_item_container celebrate_item">
+			<div class="item_title">
+				<h3>header item</h3>
+			</div>
+		</div>
+
+		<div class="aside_item_container tags_item">
+			<div class="item_title">
+				<h3>header item</h3>
+			</div>
+		</div>
 
 	</div>
 </div>
