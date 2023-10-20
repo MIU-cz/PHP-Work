2b0111faaa9d1d85c889f670fc7a8ca9a644b455
MIU-cz <0151300@gmail.com>
2023-10-20 10:45:35 +0200

    init: new test project

---
+++ fastum-dent/contact.php
@@ -0,0 +1,33 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+	$name = htmlspecialchars($_POST["contact-name"] . " " . $_POST["contact-surname"]);
+	$email = htmlspecialchars($_POST["contact-email"]);
+	$phone = htmlspecialchars($_POST["contact-phone"]);
+	$service = htmlspecialchars($_POST["contact-service"]);
+	$message = htmlspecialchars($_POST["contact-message"]);
+	// htmlspecialchars - pokud se uživatel pokusí zadat "zakázané" znaky
+
+	// my mail
+	$to = "mytaskdev@gmail.com";
+	$subject = "form request";
+
+	// massage
+	$messageBody = "Имя: $name\n";
+	$messageBody .= "Email: $email\n";
+	$messageBody .= "Телефон: $phone\n";
+	$messageBody .= "Услуга: $service\n";
+	$messageBody .= "Сообщение:\n$message";
+
+	// headers for mail
+	$headers = "From: $email" . "\r\n" .
+		"Reply-To: $email" . "\r\n" .
+		"X-Mailer: PHP/" . phpversion();
+
+	// sending mail
+	if (mail($to, $subject, $messageBody, $headers)) {
+		echo "vsechno dobre";
+	} else {
+		echo "neco jslo ne tak";
+	}
+}
